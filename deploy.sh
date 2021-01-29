4a7411c01af55c21ec7430a9fd6dbb08f00061d2
Håvard Thom <havard.thom@li-99c4e44c-24a7-11b2-a85c-caf6a98e7455.ibm.com>
2021-01-29 13:03:29 +0100

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-echo "Creating persistent volume for redis..."
-
-kubectl apply -f ./kubernetes/redis-persistent-volume.yml
-kubectl apply -f ./kubernetes/redis-persistent-volume-claim.yml
-
-echo "Creating redis deployment and service..."
-
-kubectl delete -n default deployment redis
-kubectl create -f ./kubernetes/redis-deployment.yml
-kubectl delete -n default service redis
-kubectl create -f ./kubernetes/redis-service.yml
-
 echo "Creating the my-fastapi deployment and service..."
 
 kubectl delete -n default deployment my-fastapi
@@ -23,11 +11,3 @@ echo "Adding the ingress..."
 
 minikube addons enable ingress
 kubectl apply -f ./kubernetes/minikube-ingress.yml
-
-
-# echo "Creating the vue-frontend deployment and service..."
-
-# kubectl delete -n default deployment vue-frontend
-# kubectl create -f ./kubernetes/vue-frontend-deployment.yml
-# kubectl delete -n default service vue-frontend
-# kubectl create -f ./kubernetes/vue-frontend-service.yml
